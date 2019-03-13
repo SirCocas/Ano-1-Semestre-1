24ffabcb4fba649054d9c7e83a12164cebe94c0b
Sofia Vaz <sofiateixeiravaz@ua.pt>
2019-03-13 16:31:46 +0000

    resto dos ficheiros referentes ao primeiro semestre do primeiro ano

---
+++ LI/html/labi.js
@@ -0,0 +1,8 @@
+var s=3;
+console.log(s);
+function soma(x,y)
+{
+	return x+y;
+}
+var resultado = soma(3,4);
+console.log(resultado);
---
+++ LI/html/map.js
@@ -0,0 +1,7 @@
+function DesenhaMapa() {
+    var map = new L.Map("oMeuMapa", { center: [40.633258, -8.659097], zoom: 15 });
+    var osmUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    var osmAttrib = "Map data  OpenStreetMap contributors";
+    var osm = new L.TileLayer(osmUrl, { attribution: osmAttrib });
+    map.addLayer(osm);
+}
